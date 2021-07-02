10da2e31ef63e29f042f5841230a0a200241af6d
ilimkanu <ilimkanu@gmail.com>
2021-07-02 11:26:29 +0600

    q

---
+++ src/Counter.java
@@ -0,0 +1,25 @@
+public class Counter {
+    public static void main(String[] args) {
+        do {
+            System.out.println(-10);
+            System.out.println(-9);
+            System.out.println(-8);
+            System.out.println(-7);
+            System.out.println(-6);
+            System.out.println(-4);
+            System.out.println(-3);
+            System.out.println(-2);
+            System.out.println(-1);
+            System.out.println(1);
+            System.out.println(2);
+            System.out.println(3);
+            System.out.println(4);
+            System.out.println(6);
+            System.out.println(7);
+            System.out.println(8);
+            System.out.println(9);
+            System.out.println(10);
+            break;
+        } while (true);
+    }
+}
